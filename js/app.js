9e4755539abdc6f8a4584f57fd1fa053387e3c8a
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2015-01-13 21:23:17 +0000

    chapter 1 upd

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
 audiojs.events.ready(function() {
-  var as = audiojs.createAll();
+  var as = audiojs.createAll({
+    settings: {
+      preload: false,
+    },
+    css: false,
+    createPlayer: {
+      markup: '\
+        <div class="play-pauseZ"> \
+          <p class="playZ"></p> \
+          <p class="pauseZ"></p> \
+          <p class="loadingZ"></p> \
+          <p class="errorZ"></p> \
+        </div> \
+        <div class="scrubberZ"> \
+          <div class="progressZ"></div> \
+          <div class="loadedZ"></div> \
+        </div> \
+        <div class="timeZ"> \
+          <em class="playedZ">00:00</em>/<strong class="durationZ">00:00</strong> \
+        </div> \
+        <div class="error-messageZ"></div>',
+      playPauseClass: 'play-pauseZ',
+      scrubberClass: 'scrubberZ',
+      progressClass: 'progressZ',
+      loaderClass: 'loadedZ',
+      timeClass: 'timeZ',
+      durationClass: 'durationZ',
+      playedClass: 'playedZ',
+      errorMessageClass: 'error-messageZ',
+      playingClass: 'playingZ',
+      loadingClass: 'loadingZ',
+      errorClass: 'errorZ',
+    },
+  });
 });
